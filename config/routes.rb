7c3ac607292a625d15bb0c5285f0d877a8050e61
Jordan Cutler <jordan.cutler.pa@gmail.com>
2019-06-28 21:21:22 -0400

    implemented custom scopes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'angular-items', to: 'portfolios#angular'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

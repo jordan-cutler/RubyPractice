02a8f59ec776cb948a91fe90fca131c6ade0c903
Jordan Cutler <jordan.cutler.pa@gmail.com>
2019-06-28 20:53:40 -0400

    added status updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about', to: 'pages#about'

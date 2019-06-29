b4e525a7fe6e72522c259cc5f7d8fdc895e3c4b2
Jordan Cutler <jordan.cutler.pa@gmail.com>
2019-06-28 22:46:49 -0400

    added technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,3 +54,12 @@ end
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+      name: "Technology #{technology}",
+      portfolio_id: Portfolio.last.id
+  )
+end
+
+puts "3 technologies items created"
